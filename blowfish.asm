32cb554c6ab17ba494bc93d327fd2abdc24570c1
Michael Mohr <akihana@gmail.com>
2010-12-10 13:10:08 -0800

    use defines for clarity in assembly

--- blowfish.asm
+++ blowfish.asm
@@ -3,29 +3,32 @@
 
 USE64
 
+%define PARR_LEN (16+2)*4
+%define SBOX_LEN 256*4
+
 [section .text]
 
 ; destroys registers R8, RDI, RAX
 global F
 F:
-	add rdi, 72		; skip over P -> [16 + 2] * 4 to access first S-box
+	add rdi, PARR_LEN
 	mov r8,  rsi
 	shr r8,  24
 	and r8,  0xFF
 	mov eax, [rdi+4*r8]	; y = ctx->S[0][a]
-	add rdi, 1024		; move to next S-box [256 * 4 == 1024]
+	add rdi, SBOX_LEN
 	mov r8,  rsi
 	shr r8,  16
 	and r8,  0xFF
 	add eax, [rdi+4*r8]	; y += ctx->S[1][b]
-	add rdi, 1024
+	add rdi, SBOX_LEN
 	mov r8,  rsi
 	shr r8,  8
 	and r8,  0xFF
-	xor eax, [rdi+4*r8]
-	add rdi, 1024
+	xor eax, [rdi+4*r8]	; y ^= ctx->S[2][c];
+	add rdi, SBOX_LEN
 	mov r8,  rsi
 	and r8,  0xFF
-	add eax, [rdi+4*r8]
+	add eax, [rdi+4*r8]	; y += ctx->S[3][d];
 	ret
 
